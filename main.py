bffd46d0b7eefc3c3e8f45a7613600c60b280ab3
fodyakabtw <122483398+fodyakabtw@users.noreply.github.com>
2023-11-20 00:34:33 +0300

    commit pep8

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsEllipseItem, QMainWindow
 from PyQt5.QtGui import QColor
-from PyQt5.QtCore import Qt
 from UI import Ui_MainWindow
 import random
 
@@ -13,18 +11,15 @@ class CircleApp(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.colors = ['Red', 'Orange', 'Yellow', 'Green', 'Cyan',
                        'Blue', 'Magenta', 'Purple', 'Brown', 'Black']
-
         self.scene = QGraphicsScene(self)
         self.view = QGraphicsView(self.scene)
         self.verticalLayout.addWidget(self.view)
-
         self.btnCreateCircle.clicked.connect(self.createCircle)
 
     def createCircle(self):
         diameter = random.randint(20, 100)
         x = random.randint(0, self.view.width() - diameter)
         y = random.randint(0, self.view.height() - diameter)
-
         circle = QGraphicsEllipseItem(x, y, diameter, diameter)
         circle.setBrush(QColor(random.choice(self.colors)))
         self.scene.addItem(circle)
